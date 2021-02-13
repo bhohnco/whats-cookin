e54ee43256bf727e05dbeee55d317988c6aaed9c
Matt Umland <mattumland@gmail.com>
2021-02-13 14:21:30 -0500

    Expands initial function list

--- src/scripts.js
+++ src/scripts.js
@@ -1,22 +1,28 @@
-console.log('Hello world');
 
 // FUNCTIONS
 
   function displayUserTab() {
     //reveals user tab under header and above current recipe recipe list
-    // has buttons for favorites,
+    // has buttons to that call updateRecipeList(user.toCook) or updateRecipeList(favorites)
   }
 
   function displaySearchTab() {
     //reveals search page under the header and above the current recipe list
     //list of all tags with check boxes
     //form box for typing in search terms
+    // Radio buttons for all recipes, favorites list, tocook list
     // search button
+  }
+
+  function searchRecipes() {
+    // reads data from search tab
+    // runs allRecipes.returnTagList and/or allRecipes.returnNameList
+    // or user search methods depending on form selection
   }
 
   function updateRecipeList(list) {
     // accepts an array of recipes
-    // builds markup recipe card for each item in the array
+    // builds markup recipe card for each item in the list parameter
     // injects markup into index
   }
 
