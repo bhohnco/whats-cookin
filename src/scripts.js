1e9a9b8405b5d060836ca126274c82ca3c7b5503
Matt Umland <mattumland@gmail.com>
2021-02-13 14:06:53 -0500

    Add initial function list and psuedocode

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,45 @@
 console.log('Hello world');
+
+// FUNCTIONS
+
+  function displayUserTab() {
+    //reveals user tab under header and above current recipe recipe list
+    // has buttons for favorites,
+  }
+
+  function displaySearchTab() {
+    //reveals search page under the header and above the current recipe list
+    //list of all tags with check boxes
+    //form box for typing in search terms
+    // search button
+  }
+
+  function updateRecipeList(list) {
+    // accepts an array of recipes
+    // builds markup recipe card for each item in the array
+    // injects markup into index
+  }
+
+  function displayHomePage() {
+    // hides user or search tabs
+    // updateRecipeList(allRecipes);
+  }
+
+  function pageLoad() {
+    // initalize const currentUser with getRandomUser();
+    // initlize const allRecipes = new Reciperepo(all of the recipe data)
+    // displayHomePage();
+  }
+
+  function getRandomUser() {
+    // create an instance of user from a random data
+  }
+
+
+// DOM ELEMENTS
+
+// Logo home button
+// User icon user page
+// search bar (we could replace this with a search page that allows for advanced searching)
+// Each recipe will need to be clickable, probably requires event bubbling
+// Each tag should be clickable to allow for tag search
