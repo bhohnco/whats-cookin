d477a87ab783427d2452be5de028976468b688da
bhohnco <bryanhohn13@gmail.com>
2021-02-14 10:52:44 -0700

    Start pantry class and methods

--- src/pantry.js
+++ src/pantry.js
@@ -1,25 +1,70 @@
+let ingredientsData = require('../data/ingredients.js')
 class Pantry {
   constructor(stockedIngredients) {
-    this.userPantry = stockedIngredients;
-    this.missingIngredients = [];
+    this.stockedIngredients = stockedIngredients;
   }
 
-  getPantryIngredients() {
-    this.userPantry.map(ingredient => {
-      const id = ingredient.ingredient;
-      ingredientsData.map(ingredientData => {
+  getRequiredIngredients(recipe) {
+    return recipe.ingredients.map(ingredient => {
+      const ingredientList = {};
+      const id = ingredient.id;
+      const amount = ingredient.quantity.amount;
+      ingredientsData.forEach(ingredientData => {
+        const name = ingredientData.name;
         if (ingredientData.id === id) {
-          ingredient.name =  ingredientData.name;
-          ingredient.quantity.amount = ingredientData.amount;
+          ingredientList.name = name;
+          ingredientList.amount = amount;
+        }
+      })
+      return ingredientList;
+    })
+  }
+
+  checkPantryForRequiredIngedients(recipe) {
+    let requiredIngredient = recipe.ingredients
+    let ingredientsAvailable;
+    requiredIngredient.forEach(ingredient => {
+       this.stockedIngredients.forEach(item => {
+         console.log(requiredIngredient);
+         console.log(this.stockedIngredients)
+        if (ingredient.id === item.id && item.amount >= ingredient.quantity.amount) {
+          ingredientsAvailable = true;
+        } else {
+          ingredientsAvailable = false;
+        }
+        console.log(ingredientsAvailable)
+      })
+      // return ingredientsAvailable;
+    })
+  }
+
+  provideMissingIngredients(recipe) {
+    let requiredIngredient = this.getRequiredIngredients(recipe);
+    let missingIngredients = [];
+    requiredIngredient.forEach(ingredient => {
+      this.stockedIngredients.forEach(stockedItem => {
+        if ((ingredient.id === stockedItem.ingredient) && (ingredient.amount > stockedItem.amount)) {
+          missingIngredients.push(`Ingredient: ${requiredIngredient.name} Amount: ${ingredient.amount - stockedItem.amount} `)
         }
       })
     })
-    return this.userPantry;
+    return missingIngredients;
   }
+    
+  removeStockFromPantry() {
+    
+  }
+}
+
+
 
 
 //ingredients and *amounts* of ingredient**
 //add return for ingredients needed**
 //remove ingredients when a meal is cooked take in
 // parameter of recipe and remove that list from pantry
+
+
+if (typeof module !== 'undefined') {
+  module.exports = Pantry;
 }
